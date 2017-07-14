5895d8d2a0f451774ed405bfd64d31a74d271bed
Andrei Yangabishev <waffleboot@gmail.com>
2017-07-14 17:43:00 +0300

    удалил лишнее

--- browser/ViewController.m
+++ browser/ViewController.m
@@ -1,15 +1,8 @@
-//
-//  ViewController.m
-//  browser
-//
-//  Created by Андрей on 14.07.17.
-//  Copyright © 2017 yangand. All rights reserved.
-//
 
 #import "ViewController.h"
 @import WebKit;
 
-@interface ViewController () <NSTextFieldDelegate,WKNavigationDelegate,WKUIDelegate>
+@interface ViewController () <NSTextFieldDelegate,WKUIDelegate>
 @property (nonatomic) WKWebView *webView;
 @property (nonatomic) IBOutlet NSTextField *addressTextField;
 @end
@@ -22,7 +15,6 @@
     WKWebViewConfiguration *webConfiguration = [[WKWebViewConfiguration alloc] init];
     self.webView = [[WKWebView alloc] initWithFrame:frame configuration:webConfiguration];
     self.webView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
-    self.webView.navigationDelegate = self;
     self.webView.UIDelegate = self;
     [self.view addSubview:self.webView];
     self.addressTextField.delegate = self;
@@ -49,38 +41,6 @@
     self.addressTextField.stringValue = request.URL.absoluteString;
     [self.webView loadRequest:request];
     return nil;
-}
-
-//- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
-//    decisionHandler(WKNavigationActionPolicyAllow);
-//}
-
-- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler {
-    decisionHandler(WKNavigationResponsePolicyAllow);
-}
-
-- (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error {
-    
-}
-
-- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
-    
-}
-
-- (void)webView:(WKWebView *)webView didReceiveServerRedirectForProvisionalNavigation:(WKNavigation *)navigation {
-    
-}
-
-- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation {
-    
-}
-
-- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
-    
-}
-
-- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation withError:(NSError *)error {
-    NSLog(@"%@", error);
 }
 
 @end
