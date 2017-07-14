560007656882e8af126d57fdc48b2a1614a602e4
Andrei Yangabishev <waffleboot@gmail.com>
2017-07-14 17:53:08 +0300

    добавил очистку тэгов

--- browser/ViewController.m
+++ browser/ViewController.m
@@ -2,7 +2,7 @@
 #import "ViewController.h"
 @import WebKit;
 
-@interface ViewController () <NSTextFieldDelegate,WKUIDelegate>
+@interface ViewController () <NSTextFieldDelegate,WKUIDelegate,WKScriptMessageHandler>
 @property (nonatomic) WKWebView *webView;
 @property (nonatomic) IBOutlet NSTextField *addressTextField;
 @end
@@ -12,12 +12,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     CGRect frame = CGRectMake(0, 0, 480, 233);
-    WKWebViewConfiguration *webConfiguration = [[WKWebViewConfiguration alloc] init];
-    self.webView = [[WKWebView alloc] initWithFrame:frame configuration:webConfiguration];
+    self.webView = [[WKWebView alloc] initWithFrame:frame configuration:[self webConfiguration]];
     self.webView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
     self.webView.UIDelegate = self;
     [self.view addSubview:self.webView];
     self.addressTextField.delegate = self;
+
+}
+
+- (WKWebViewConfiguration *)webConfiguration {
+    NSError *error;
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"script" ofType:@"js"];
+    NSString *text = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:&error];
+    WKWebViewConfiguration *webConfiguration = [[WKWebViewConfiguration alloc] init];
+    if (text) {
+        WKUserScript *script = [[WKUserScript alloc] initWithSource:text
+                                                      injectionTime:WKUserScriptInjectionTimeAtDocumentEnd
+                                                   forMainFrameOnly:YES];
+        WKUserContentController *controller = [[WKUserContentController alloc] init];
+        [controller addScriptMessageHandler:self name:@"app"];
+        [controller addUserScript:script];
+        webConfiguration.userContentController = controller;
+    }
+    return webConfiguration;
 }
 
 - (BOOL)control:(NSControl *)control
@@ -41,6 +58,10 @@
     self.addressTextField.stringValue = request.URL.absoluteString;
     [self.webView loadRequest:request];
     return nil;
+}
+
+- (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message {
+//    NSLog(@"%@", message.body);
 }
 
 @end
---
+++ browser/script.js
@@ -0,0 +1,22 @@
+
+function handleClickEvent(event){
+    const target = event.target
+    if(!target) {
+        return
+    }
+    target.parentNode.removeChild(target)
+    window.webkit.messageHandlers.app.postMessage(document.documentElement.outerHTML.toString())
+}
+
+Array.from(document.querySelectorAll('p')).forEach(function (el) {
+                                                   el.addEventListener('click', function(event) {
+                                                                       event.preventDefault()
+                                                                       handleClickEvent(event)
+                                                                       }, false)
+                                                   })
+Array.from(document.querySelectorAll('div')).forEach(function (el) {
+                                                     el.addEventListener('click', function(event) {
+                                                                         event.preventDefault()
+                                                                         handleClickEvent(event)
+                                                                         }, false)
+                                                     })
